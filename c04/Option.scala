e0dd4cf88137427a991c71243f19905474266687
henky <hanky.adhi.chandra@ansvia.com>
2017-06-21 21:02:52 +0700

    lifting

--- c04/Option.scala
+++ c04/Option.scala
@@ -1,4 +1,4 @@
-//{ echo ":paste" & cat Option.scala & echo ":q";}| scala
+//{echo ":paste" & cat Option.scala & echo ":q";}| scala
 
 import scala.{Option => _, Either => _, _}
 case object None extends Option[Nothing]
@@ -52,10 +52,12 @@ sealed trait Option[+A]{
 				}
 		}
 	}
+	
 }
 
 object Option{
 }
+
 
 val a0 = Some(3)
 val a1 = a0.map(_ * 3)
@@ -90,3 +92,41 @@ def variance(xs: Seq[Double]): Option[Double] = {
 val lDouble = List(1.1, 0.0, 3.5, 2.2, 4.1, 7.1, 9,2)
 val m = mean (lDouble)
 val v = variance(lDouble)
+
+def lift[A,B](f: A=>B): Option[A] => Option[B] = _ map f
+
+val abs0 = lift(math.abs)
+
+val animals = List("cat", "dog", "snake")
+
+animals.lift(0)
+animals.lift(7) 
+
+def insuranceRateQuote(age:Int, numberOfSpeedingTicket: Int): Double = {
+	age * 0.01 + numberOfSpeedingTicket * 0.015
+}
+
+def Try[A](a: => A): Option[A] = 
+	try Some(a)
+	catch {case e: Exception => None}
+
+
+def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+	(a, b) match {
+		case (Some(x), Some(y)) =>
+			Some(f(x, y))
+		case _ =>
+			None
+	}
+}
+
+def parseInsuranceRateQuote (age: String, numberOfSpeedingTickets: String) = {
+	val optAge: Option[Int] = Try(age.toInt)
+	val optTickets : Option[Int] = Try(numberOfSpeedingTickets.toInt)
+	map2(optAge, optTickets)(insuranceRateQuote)
+}
+
+
+val inv = parseInsuranceRateQuote("100", "3y")
+val vld = parseInsuranceRateQuote("100", "3")
+
