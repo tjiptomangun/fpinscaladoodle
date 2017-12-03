676abdd6751b2b7723f34a1b778c9683be5a6363
tjiptomangun <tjiptomangun@yahoo.com>
2017-12-03 17:43:26 +0700

    Stream drop

--- c05/Stream.scala
+++ c05/Stream.scala
@@ -36,7 +36,44 @@ sealed trait Stream[+A] {
 
 		inner(this, k);
 		
+	}//end of take
+
+	def drop(k: Int): Stream[A] = {
+		def inner(str: Stream[A], n: Int): Stream[A] = {
+			if (n <= 0){
+				str
+			}
+			else {
+				str match {
+					case Cons(a, b) =>
+						inner(b(), n - 1)
+					case _ =>
+						Stream.empty[A]
+				}
+			}
+		}
+		inner(this, k)
 	}
+
+	def takeWhile(p: A => Boolean):Stream[A] = {
+		def inner(str :  Stream[A], n: Int): Stream[A] = {
+			if (n > 0){
+				str match {
+					case Cons(a, b) =>
+						Stream.cons(a(), inner(b(), n - 1))
+	
+					case _ =>
+						Stream.empty[A]
+
+				}
+			}
+			else
+				Stream.empty[A]
+		}
+
+		inner(this, k);
+	}
+		
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A] (h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -49,26 +86,40 @@ object Stream {
 	}
 
 	def cons2[A](hd: => A, tl: => Stream[A]): Stream[A] = {
-		val head = hd;
-		val tail = tl;
-		Cons(() => head, () => tail) 
+		Cons(() => hd, () => tl) 
 	}
 	
 	def empty[A]: Stream[A] = Empty
 
 	def apply[A] (as: A*): Stream[A] =
 		if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+		//if (as.isEmpty) empty else cons2(as.head, apply(as.tail: _*))
 
 }
 
-val d = List({println ("zee"); 1}, {println ("wee"); 3}, 5, 6, 2, 4, 11, 32, 12, 7)
-val r0 = Stream(d:_*)
+def fn (): Int= {
+	println ("Hello World")
+	1;	
+}
+
+val d = fn _
+val ld = List(d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d)
+
+val r0 = Stream(ld:_*)
 val r1 = r0.toList
 
 val c = Cons(()=>{println("Hello"); 1}, ()=>Stream.empty)
 c.headOption
 c.headOption
 
-val e = Stream.cons({Thread.sleep(3000); 1}, Stream.empty)
+val e = Stream.cons({println("goodbye cons!"); Thread.sleep(3000); 1}, Stream.empty)
+val f = Stream.cons2({println("goodbye cons2!"); Thread.sleep(3000); 1}, Stream.empty)
 
 //val f = Stream.cons2({Thread.sleep(3000); 1}, Stream.empty)
+
+val g0 = List(1, 2, 3, 4, 5, 6)
+val g1 = Stream(g0:_*)
+val g2 = g1.toList
+val g3 = g1.take(2).toList
+val g4 = g1.drop(2).toList
+
--- c05/test.scala
+++ c05/test.scala
@@ -42,8 +42,8 @@ xnif2(true , 3, 4,
 
 def mayBeTwice(b: Boolean, i: => Int) = if (b) i + i else 0
 
-val t01 = mayBeTwice(true, {println("hi"); 1 + 32})
-val t02 = mayBeTwice(false, {println("hi"); 1 + 32})
+val t01 = mayBeTwice(true, {println("hi"); 1 + 29})
+val t02 = mayBeTwice(false, {println("hi"); 1 + 30})
 
 def xmayBeTwice(b: Boolean, i: (Int) => Int):Int = {
 	if (b)
@@ -52,26 +52,40 @@ def xmayBeTwice(b: Boolean, i: (Int) => Int):Int = {
 		i(3) - 2
 }
 
-val t03 = xmayBeTwice(true, (x: Int) => {println("hi"); 1 + 32})
+val t03 = xmayBeTwice(true, (x: Int) => {println("hi"); 1 + 31})
 val t04 = xmayBeTwice(false,(x: Int) => {println("hi"); 1 + 32})
 
-def ymayBeTwice(b: Boolean, i: => Int) = {
+def yNotTwice(b: Boolean, i: => Int) = {
 	lazy val j = i
 	if (b) j + j  else 0
 }
 
-val t05 = ymayBeTwice(true, {println("hi"); 1 + 32})
-val t06 = ymayBeTwice(false, {println("hi"); 1 + 32})
+val t05 = yNotTwice(true, {println("hi"); 1 + 33})
+val t06 = yNotTwice(false, {println("hi"); 1 + 34})
 
-def zmayBeTwice(b: Boolean, i: => Int) = {
-	val j = i
+def zNotTwice(b: Boolean, i: () => Int) = {
+	val j = i()
 	if (b) j + j  else 0
 }
 
-val t07 = zmayBeTwice(true, {println("hi"); 1 + 32})
-val t08 = zmayBeTwice(false, {println("hi"); 1 + 32})
+val t07 = zNotTwice(true, () => {println("hi"); 1 + 35})
+val t08 = zNotTwice(false, () => {println("hi"); 1 + 36})
 
+def cccc(f: => Int) = {f}
+def dddd(f:()  => Int) = {1}
+def eeee(f:()  => Int):Int = {f()}
+cccc(4)
+dddd(()=>2)
+cccc(Math.pow(Math.pow(3, 2.5), 2.545).toInt)
+dddd(() => Math.pow(Math.pow(3, 2.5), 2.545).toInt)
+eeee(() => Math.pow(Math.pow(3, 2.5), 2.545).toInt)
 
-
+/* 
+ * Notes
+ * x: () => A and x: => A are different on calling style and signature but 
+ * do exactly the same thing.
+ * It is just mean that in first form we need to have () in applying
+ * and no need to have () in second form of applying
+ */
 
 
