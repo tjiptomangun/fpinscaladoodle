e16161be0c137f565de0608dbf22187f346e6a6f
tjiptomangun <tjiptomangun@yahoo.com>
2018-12-13 06:44:52 +0700

    scream list

--- c05/Scream.scala
+++ c05/Scream.scala
@@ -2,9 +2,25 @@
  * Second try on lazyness
  */
 
-sealed trait Scream[+A]
+sealed trait Scream[+A] {
+	def headOption: Option[A] = this match {
+		case Empty => None
+		case Cons(h, t) => Some(h())
+	}
+
+	def toList: List[A] = {
+		this match {
+			case Cons(h, t) =>
+				h()::t.toList
+			case _ =>
+				List.empty[A]
+		}	
+	}
+
+}
 case object Empty extends Scream[Nothing]
-case class Cons[+A](h: () => A, t: () => Scream[A]) extends Scream[A]
+case class Cons[+A] (h: () => A, t: () => Scream[A]) extends Scream[A]
+
 
 object Scream {
 	def cons[A] (hd: => A, tl: => Scream[A]): Scream[A] = {
