d443f96f8538e1fa54a4e99fe3d0177637bfd7c6
tjiptomangun <tjiptomangun@yahoo.com>
2019-01-19 10:36:43 +0700

    scream take

--- c05/Scream.scala
+++ c05/Scream.scala
@@ -8,13 +8,82 @@ sealed trait Scream[+A] {
 		case Cons(h, t) => Some(h())
 	}
 
-	def toList: List[A] = {
+	//simple but not tailrec	
+	def toListNT: List[A] = {
 		this match {
 			case Cons(h, t) =>
-				h()::t.toList
+				h()::t().toListNT
 			case _ =>
 				List.empty[A]
 		}	
+	}
+
+	
+	def toList: List[A] = {
+	
+		@annotation.tailrec
+		def inner(a:Scream[A], accum:List[A]): List[A] = {
+			a match {
+				case Cons(h, t) =>
+					inner(t(), accum :+ h())
+				case _ =>
+					accum
+			}
+		} 
+		inner(this, List.empty[A])
+	}
+
+	def take(n: Int)  : Scream[A] = {
+		n match {
+			case 0 =>
+				Scream.empty
+			case _ =>
+				this match {
+					case Cons(h, t) =>
+						Scream.cons(h(), t().take(n - 1))
+					case _ =>
+						Scream.empty 
+				}
+		}
+		
+	}
+
+	def drop(n: Int) : Scream[A] = {
+		n match {
+			case 0 =>
+				this
+			case _ =>
+				this match {
+					case Cons(h, t) =>
+						t().drop(n - 1)
+					case _ =>
+						Scream.empty
+				}
+		}
+	}
+
+	def takeWhile(p: A => Boolean): Scream[A] = {
+		this match {
+			case Empty =>
+				Scream.empty
+			case Cons(h, t) =>
+				p(h()) match {
+					case true =>
+						Scream.cons(h(), t().takeWhile(p))
+					case _ =>
+						Scream.empty
+				}
+		}
+	}
+
+	def exists(p: A => Boolean): Boolean = this match {
+		case Empty =>
+			false
+		case Cons(h, t) =>
+			if (p(h()))
+				true
+			else
+				t().exists(p)
 	}
 
 }
